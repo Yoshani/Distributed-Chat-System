d84f8359359c7b0c0ba30412703a9080038204a7
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2021-10-07 04:09:44 +0530

    Refactor json obj name

--- src/main/java/server/ServerMessage.java
+++ src/main/java/server/ServerMessage.java
@@ -1,62 +1,63 @@
 package server;
 
 import org.json.simple.JSONObject;
+
 import java.util.*;
 
 public class ServerMessage {
 
     @SuppressWarnings("unchecked")
     public static JSONObject getApprovalNewID(String approve) {
-        JSONObject join = new JSONObject();
-        join.put("type", "newidentity");
-        join.put("approved", approve);
-        return join;
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("type", "newidentity");
+        jsonObject.put("approved", approve);
+        return jsonObject;
     }
 
     @SuppressWarnings("unchecked")
     public static JSONObject getRoomChange(String id, String MainHall) {
-        JSONObject join = new JSONObject();
-        join.put("type", "roomchange");
-        join.put("identity", id);
-        join.put("former","");
-        join.put("roomid",MainHall);
-        return join;
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("type", "roomchange");
+        jsonObject.put("identity", id);
+        jsonObject.put("former", "");
+        jsonObject.put("roomid", MainHall);
+        return jsonObject;
     }
 
     @SuppressWarnings("unchecked")
     public static JSONObject getCreateRoom(String id, String approve) {
-        JSONObject join = new JSONObject();
-        join.put("type", "createroom");
-        join.put("roomid", id);
-        join.put("approved",approve);
-        return join;
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("type", "createroom");
+        jsonObject.put("roomid", id);
+        jsonObject.put("approved", approve);
+        return jsonObject;
     }
 
     @SuppressWarnings("unchecked")
     public static JSONObject getCreateRoomChange(String id, String former, String roomID) {
-        JSONObject join = new JSONObject();
-        join.put("type", "roomchange");
-        join.put("identity", id);
-        join.put("former",former);
-        join.put("roomid",roomID);
-        return join;
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("type", "roomchange");
+        jsonObject.put("identity", id);
+        jsonObject.put("former", former);
+        jsonObject.put("roomid", roomID);
+        return jsonObject;
     }
 
     @SuppressWarnings("unchecked")
     public static JSONObject getWho(String roomID, List<String> participants, String id) {
-        JSONObject join = new JSONObject();
-        join.put("type", "roomcontents");
-        join.put("roomid", roomID);
-        join.put("identities",participants);
-        join.put("owner",id);
-        return join;
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("type", "roomcontents");
+        jsonObject.put("roomid", roomID);
+        jsonObject.put("identities", participants);
+        jsonObject.put("owner", id);
+        return jsonObject;
     }
 
     @SuppressWarnings("unchecked")
     public static JSONObject getList(List<String> rooms) {
-        JSONObject join = new JSONObject();
-        join.put("type", "roomlist");
-        join.put("rooms",rooms);
-        return join;
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("type", "roomlist");
+        jsonObject.put("rooms", rooms);
+        return jsonObject;
     }
 }

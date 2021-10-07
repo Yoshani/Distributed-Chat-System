e5ddde4bf7d2fe5462c44d4ab81ecfa0e51871c7
Chiran-Siriwardana <47823522+chiransiriwardhana@users.noreply.github.com>
2021-10-07 21:24:32 +0530

    Update Main.java

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("LOG  : ------server started------");
+        System.out.println("Failure detection");
 
         ServerState.getInstance().initializeWithConfigs(args[0], args[1]);
         try {

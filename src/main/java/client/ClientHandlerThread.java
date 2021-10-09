5d44a450bc22aab89fb53ac93b9b19acd486bd0a
Sachini-Dissanayaka <dmsachiniacc@gmail.com>
2021-10-09 20:47:06 +0530

    changes - message

--- src/main/java/client/ClientHandlerThread.java
+++ src/main/java/client/ClientHandlerThread.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Arrays;
+import static messaging.MessageTransfer.*;
 
 public class ClientHandlerThread extends Thread {
 
@@ -25,17 +26,12 @@ public class ClientHandlerThread extends Thread {
     private Long threadID;
     private int approvedClientID = -1;
     private int approvedRoomCreation = -1;
-    final Object lock;
 
-    //TODO : check input stream local var
-    private DataOutputStream dataOutputStream;
-
-    public ClientHandlerThread( Socket clientSocket ) {
+    public ClientHandlerThread(Socket clientSocket) {
         String serverID = ServerState.getInstance().getServerID();
         ServerState.getInstance().getRoomMap().put("MainHall-" + serverID, ServerState.getInstance().getMainHall());
 
         this.clientSocket = clientSocket;
-        this.lock = new Object();
     }
 
     public void setThreadID(Long threadID) {
@@ -50,34 +46,6 @@ public class ClientHandlerThread extends Thread {
         this.approvedRoomCreation = approvedRoomCreation;
     }
 
-    public Object getLock() {
-        return lock;
-    }
-
-    //check the existence of a key in json object
-    private boolean hasKey(JSONObject jsonObject, String key) {
-        return (jsonObject != null && jsonObject.get(key) != null);
-    }
-
-    //check validity of the ID
-    private boolean checkID(String id) {
-        return (Character.toString(id.charAt(0)).matches("[a-zA-Z]+") && id.matches("[a-zA-Z0-9]+") && id.length() >= 3 && id.length() <= 16);
-    }
-
-    //send broadcast message
-    private void sendBroadcast(JSONObject obj, ArrayList<Socket> socketList) throws IOException {
-        for (Socket each : socketList) {
-            Socket TEMP_SOCK = (Socket) each;
-            PrintWriter TEMP_OUT = new PrintWriter(TEMP_SOCK.getOutputStream());
-            TEMP_OUT.println(obj);
-            TEMP_OUT.flush();
-        }
-    }
-    //send message to client
-    private void send(JSONObject obj) throws IOException {
-        dataOutputStream.write((obj.toJSONString() + "\n").getBytes(StandardCharsets.UTF_8));
-        dataOutputStream.flush();
-    }
 
     //format message before sending it to client
     private void messageSend(ArrayList<Socket> socketList, String msg, List<String> msgList) throws IOException {
@@ -85,25 +53,25 @@ public class ClientHandlerThread extends Thread {
         String[] array = msg.split(" ");
         if (array[0].equals("newid")) {
             sendToClient = ClientMessage.getApprovalNewID(array[1]);
-            send(sendToClient);
+            sendClient(sendToClient,clientSocket);
         } else if (array[0].equals("roomchange")) {
             sendToClient = ClientMessage.getJoinRoom(array[1], array[2].replace("_", ""), array[3]);
             sendBroadcast(sendToClient, socketList);
         } else if (array[0].equals("createroom")) {
             sendToClient = ClientMessage.getCreateRoom(array[1], array[2]);
-            send(sendToClient);
+            sendClient(sendToClient,clientSocket);
         } else if (array[0].equals("roomchangeall")) {
             sendToClient = ClientMessage.getCreateRoomChange(array[1], array[2], array[3]);
             sendBroadcast(sendToClient, socketList);
         } else if (array[0].equals("roomcontents")) {
             sendToClient = ClientMessage.getWho(array[1], msgList, array[2]);
-            send(sendToClient);
+            sendClient(sendToClient,clientSocket);
         } else if (array[0].equals("roomlist")) {
             sendToClient = ClientMessage.getList(msgList);
-            send(sendToClient);
+            sendClient(sendToClient,clientSocket);
         } else if (array[0].equals("deleteroom")) {
             sendToClient = ClientMessage.getDeleteRoom(array[1], array[2]);
-            send(sendToClient);
+            sendClient(sendToClient,clientSocket);
         } else if (array[0].equals("message")) {
             sendToClient = ClientMessage.getMessage(array[1], String.join(" ",Arrays.copyOfRange(array, 2, array.length)));
             sendBroadcast(sendToClient, socketList);
@@ -118,44 +86,37 @@ public class ClientHandlerThread extends Thread {
             while(!LeaderState.getInstance().isLeaderElected()) { // TODO: any better way to do this?
                 Thread.sleep(1000);
             }
-            synchronized( lock ) {
-                while( approvedClientID == -1 )
-                {
-                    // if self is leader get direct approval
-                    if( LeaderState.getInstance().isLeader() )
+            while( approvedClientID == -1 ) {
+                // if self is leader get direct approval
+                if( LeaderState.getInstance().isLeader() ) {
+                    approvedClientID = LeaderState.getInstance().isClientIDAlreadyTaken( clientID ) ? 0 : 1;
+                } else {
+                    try
                     {
-                        approvedClientID = LeaderState.getInstance().isClientIDAlreadyTaken( clientID ) ? 0 : 1;
-                    }
-                    else
-                    {
-                        try
-                        {
-                            // send client id approval request to leader
-                            MessageTransfer.sendToLeader(
-                                    ServerMessage.getClientIdApprovalRequest( clientID,
-                                            String.valueOf( ServerState.getInstance().getSelfID() ),
-                                            String.valueOf( threadID )
-                                    )
-                            );
+                        // send client id approval request to leader
+                        MessageTransfer.sendToLeader(
+                                ServerMessage.getClientIdApprovalRequest( clientID,
+                                        String.valueOf( ServerState.getInstance().getSelfID()),
+                                        String.valueOf( threadID )
+                                )
+                        );
 
-                            System.out.println( "INFO : Client ID '" + clientID + "' sent to leader for approval" );
-                        }
-                        catch( Exception e )
-                        {
-                            e.printStackTrace();
-                        }
-                        lock.wait(7000);
+                        System.out.println( "INFO : Client ID '" + clientID + "' sent to leader for approval" );
                     }
+                    catch( Exception e )
+                    {
+                        e.printStackTrace();
+                    }
+                    Thread.sleep( 7000 );
                 }
             }
-
             if( approvedClientID == 1 ) {
                 System.out.println( "INFO : Received correct ID ::" + jsonStringFromClient );
                 this.clientState = new ClientState( clientID, ServerState.getInstance().getMainHall().getRoomID(),
                         connected.getPort(), connected );
                 ServerState.getInstance().getMainHall().addParticipants( clientState );
 
-                // create broadcast list
+                //create broadcast list
                 String mainHallRoomID = ServerState.getInstance().getMainHall().getRoomID();
                 HashMap<String,ClientState> mainHallClientList = ServerState.getInstance()
                                                                             .getRoomMap()
@@ -171,14 +132,12 @@ public class ClientHandlerThread extends Thread {
                 synchronized( connected )
                 {
                     messageSend( null, "newid true", null );
-                    messageSend( socketList, "roomchange " + clientID + " _" + " MainHall-" +
-                                                     ServerState.getInstance().getServerID(), null );
+                    messageSend( socketList, "roomchange " + clientID + " _" + " MainHall-" + ServerState.getInstance().getServerID(), null );
                 }
             }  else if( approvedClientID == 0 ) {
                 System.out.println("WARN : ID already in use");
                 messageSend(null, "newid false", null);
             }
-            approvedClientID = -1;
         } else {
             System.out.println("WARN : Recieved wrong ID type");
             messageSend(null, "newid false", null);
@@ -214,39 +173,31 @@ public class ClientHandlerThread extends Thread {
             while(!LeaderState.getInstance().isLeaderElected()) {
                 Thread.sleep(1000);
             }
-
-            synchronized( lock )
-            {
-                while( approvedRoomCreation == -1 )
-                {
-                    // if self is leader get direct approval
-                    if( LeaderState.getInstance().isLeader() )
+            while( approvedRoomCreation == -1 ) {
+                // if self is leader get direct approval
+                if( LeaderState.getInstance().isLeader() ) {
+                    approvedRoomCreation = LeaderState.getInstance()
+                                                      .isRoomCreationApproved( clientState.getClientID(), newRoomID ) ? 1 : 0;
+                } else {
+                    try
                     {
-                        approvedRoomCreation = LeaderState.getInstance()
-                                                          .isRoomCreationApproved( clientState.getClientID(), newRoomID ) ? 1 : 0;
-                    }
-                    else
-                    {
-                        try
-                        {
-                            // send room creation approval request to leader
-                            MessageTransfer.sendToLeader(
-                                    ServerMessage.getRoomCreateApprovalRequest( clientState.getClientID(),
-                                            newRoomID,
-                                            String.valueOf( ServerState.getInstance().getSelfID() ),
-                                            String.valueOf( threadID )
-                                    )
-                            );
+                        // send room creation approval request to leader
+                        MessageTransfer.sendToLeader(
+                                ServerMessage.getRoomCreateApprovalRequest( clientState.getClientID(),
+                                        newRoomID,
+                                        String.valueOf( ServerState.getInstance().getSelfID()),
+                                        String.valueOf( threadID )
+                                )
+                        );
 
-                            System.out.println( "INFO : Room '" + newRoomID + "' create request by '"
-                                                        + clientState.getClientID() + "' sent to leader for approval" );
-                        }
-                        catch( Exception e )
-                        {
-                            e.printStackTrace();
-                        }
-                        lock.wait( 7000 );
+                        System.out.println( "INFO : Room '" + newRoomID + "' create request by '"
+                                                    + clientState.getClientID() + "' sent to leader for approval" );
                     }
+                    catch( Exception e )
+                    {
+                        e.printStackTrace();
+                    }
+                    Thread.sleep( 7000 );
                 }
             }
             if( approvedRoomCreation == 1) {
@@ -283,7 +234,6 @@ public class ClientHandlerThread extends Thread {
                 System.out.println("WARN : Room id already in use or client already owns a room");
                 messageSend(null, "createroom " + newRoomID + " false", null);
             }
-            approvedRoomCreation = -1;
         } else {
             System.out.println("WARN : Recieved wrong room ID type");
             messageSend(null, "createroom " + newRoomID + " false", null);
@@ -369,7 +319,7 @@ public class ClientHandlerThread extends Thread {
         }
     }
 
-    // quit
+    //quit room
     private void quit(Socket connected,String jsonStringFromClient) throws IOException {
 
         String roomID = clientState.getRoomID();
@@ -446,9 +396,6 @@ public class ClientHandlerThread extends Thread {
 
             BufferedReader bufferedReader = new BufferedReader(
                     new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
-
-            this.dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
-
 
             while (true) {
 
